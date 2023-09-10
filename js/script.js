3b62a0b9843cef8379fe81e35dab9c5e934cff6a
Sumeet Supali <77237460+Sumeet-surya@users.noreply.github.com>
2023-09-10 17:59:24 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,85 @@
+// import images as relative image path won't work with vite/vercel.
+import check from '../assets/check.svg'
+import star from '../assets/star.svg'
+import sushi12 from '../assets/sushi-12.png'
+import sushi11 from '../assets/sushi-11.png'
+import sushi10 from '../assets/sushi-10.png'
+
+
+
+
+
+
+
+
+// Import AOS and its CSS
+import AOS from 'aos';
+
+import 'aos/dist/aos.css';
+
+// Initialize AOS with configuration options
+AOS.init({
+  duration: 1000,
+  offset:100, // Animation duration in milliseconds
+  easing: 'ease-in-out', // Easing function for animations
+  once: true, // Whether animations should only occur once
+});
+
+// Apply AOS animations to specific elements
+AOS.refresh(); // Refresh AOS animations (if needed)
+
+// In your HTML, add the 'data-aos' attribute to elements you want to animate
+// Example:
+// <div data-aos="fade-up">Content to animate</div>
+
+
+
+
+
+
+
+
+
+
+const trendingSushis = [
+    'Make Sushi',
+    'Nigiri Sushi',
+    'Oshizushi',
+    'Temaki Sushi',
+    'Uramaki Sushi',
+    'Inari Sushi'
+];
+
+const trendingDrinks = [
+    "Oruncha",
+    "Ofukucha",
+    "Sakura Tea",
+    "Kombu-cha",
+    "Aojiru",
+    "Mugicha",
+]
+
+const cards = [
+    {
+        imgSrc: sushi12,
+        alt: "sushi-12",
+        title: "Chezu Sushi",
+        rating: "4.8",
+        price: "$21.00"
+    },
+    {
+        imgSrc: sushi11,
+        alt: "sushi-11",
+        title: "Originale Sushi",
+        rating: "4.8",
+        price: "$21.00",
+        active: true
+    },
+    {
+        imgSrc: sushi10,
+        alt: "sushi-10",
+        title: "Ramen Legendo",
+        rating: "4.8",
+        price: "$21.00"
+    }
+];
